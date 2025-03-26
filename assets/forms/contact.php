6b0766fd8e3fffa7d3d3bb3ba0d54783d6a6fb5f
Wanawan-bismillahsemangat <dzakwanarya45@gmail.com>
2025-03-26 20:14:09 +0700

    +++++++++++++++++++++

--- assets/forms/contact.php
+++ assets/forms/contact.php
@@ -1,12 +1,5 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
+ 
   $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -15,7 +8,7 @@
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
-  $contact = new PHP_Email_Form;
+  $contact = new $PHP_Email_Form;
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
